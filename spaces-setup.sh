3f1066c9d3301354f8e75ddb565f730d5f2f0e53
LES MATHESON <LMATHESON4@bloomberg.net>
2023-07-05 17:59:51 +0000

    Sync (auto)

--- spaces-setup.sh
+++ spaces-setup.sh
@@ -4,6 +4,6 @@
 
 curl http://s3.dev.obdc.bcs.bloomberg.com/shellkit-data/bb-shellkit-bootstrap.sh | bash -
 
-${HOME}/.local/bin/shpm install vbase makeup
+${HOME}/.local/bin/shpm install vbase
 
 
