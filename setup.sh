bf08c5fd2eab855b3f82918590d6de63071217ce
LES MATHESON <LMATHESON4@bloomberg.net>
2023-07-16 13:00:55 +0000

    Fix spelling of bbshellkit

--- setup.sh
+++ setup.sh
@@ -10,5 +10,5 @@ initPrimary() {
 if [[ -z "$sourceMe" ]]; then
     # Install jumpstart fresh:
     curl --noproxy '*' http://s3.dev.obdc.bcs.bloomberg.com/shellkit-data/jumpstart-setup-latest.sh -o ~/jumpstart-$UID-$$ && bash ~/jumpstart-$UID-$$ && rm -f ~/jumpstart-$UID-$$
-    bash -ic 'source ~/dotfiles/jumpstart.bashrc; __jmpstart_add bb-shellkit vbase'
+    bash -ic 'source ~/dotfiles/jumpstart.bashrc; __jmpstart_add bbshellkit vbase'
 fi
