6d21b69cc4dc49c78ee09a306498b8f8cb423e3d
Les Matheson <lmatheson4@bloomberg.net>
2023-12-03 08:39:16 -0500

    Cleanup vbase.mk to use shpm directly instead of jumpstart

--- inc/vbase.mk
+++ inc/vbase.mk
@@ -11,8 +11,19 @@ $(Flag)/vbase: $(Finit) $(Flag)/jumpstart
 		cd ~/tmp
 		curl -L https://github.com/sanekits/shellkit-pm/releases/download/0.9.1/shellkit-bootstrap.sh -o ./shellkit-bootstrap.sh
 		bash ./shellkit-bootstrap.sh
-		bash -lic 'JUMPSTART_FORCE_YES=1 jumpstart add vbase'
+		{
+			cat <<-EOF
+			set +ue
+			for package in $$(shellkit-query-package.sh vbase | awk '/vbase.virtual/'); do
+				[[ "$$package" == vbase.virtual ]] && continue
+				$${HOME}/.local/bin/shpm install "$$package"
+			done
+			set -ue
+			$${HOME}/.local/bin/vi-mode.sh on
+			echo "jumpstart vbase added OK"
+			exit
+			EOF
+		} | bash -li /dev/stdin
 	}
-	bash -lic 'vi-mode.sh on'
 	echo 'alias d=dirs' >> $(HOME)/.cdpprc
 	touch $@
