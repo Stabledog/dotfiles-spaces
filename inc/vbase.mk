55caf28ed54939d50ae9bfa2cfbed7d31c62be1b
LES MATHESON <LMATHESON4@bloomberg.net>
2023-12-07 18:44:28 +0000

    Rework vbase.mk to use common logic

--- inc/vbase.mk
+++ inc/vbase.mk
@@ -3,27 +3,35 @@ inc/vbase.mk: ;
 vbase: $(Flag)/vbase
 $(Flag)/vbase: $(Finit) $(Flag)/jumpstart
 	@# Install shellkit vbase collection via jumpstart
-	$(ISBB) && {
-		bash -lic 'JUMPSTART_FORCE_YES=1 jumpstart add vbase; exit;'
-	} || {
-		# Fallback to public shpm:
-		mkdir -p ~/tmp
-		cd ~/tmp
-		curl -L https://github.com/sanekits/shellkit-pm/releases/download/0.9.1/shellkit-bootstrap.sh -o ./shellkit-bootstrap.sh
-		bash ./shellkit-bootstrap.sh
-		{
-			tmpfile=$(VHOME)/.tmp-vbase-$$$$
-			cat <<-EOF > "$$tmpfile"
-			set +ue
-			for package in $$(shellkit-query-package.sh vbase | awk '/vbase.virtual/'); do
-				[[ "$$package" == vbase.virtual ]] && continue
-				$(VHOME)/.local/bin/shpm install "$$package"
-			done
-			exit
-			EOF
-		}
-		bash -c "$$tmpfile"
-	}
+	mkdir -p ~/tmp
+	cd ~/tmp
+	set -x
+	if $(ISBB); then
+		bootstrap_url=https://s3.dev.bcs.bloomberg.com/shellkit-data/bb-shellkit-bootstrap.sh
+	else
+		bootstrap_url=https://github.com/sanekits/shellkit-pm/releases/download/0.9.1/shellkit-bootstrap.sh
+	fi
+	curl -k -L "$$bootstrap_url" -o ./shellkit-bootstrap.sh
+	bash ./shellkit-bootstrap.sh
+
+	tmpfile=$(VHOME)/.tmp-vbase-$$$$
+	cat <<-"EOF" | cut -c 2- > "$$tmpfile"
+	|set +ue
+	|set -x
+	|tmpfile_A=$(VHOME)/.tmp-vbase-$$$$._A
+	|$(VHOME)/.local/bin/shellkit-query-package.sh vbase | tee  "$$tmpfile_A"
+	|packages=( $$( awk '/vbase.virtual/' "$$tmpfile_A"  ) )
+	|for package in "$${packages[@]}"; do
+	|	[[ "$$package" == vbase.virtual ]] && continue
+	|	$(VHOME)/.local/bin/shpm install "$$package"
+	|done
+	|rm "$$tmpfile_A" || :
+	|exit
+	EOF
+
+	bash -x "$$tmpfile"
+	rm "$$tmpfile" || :
+
 	set -ue
 	$(VHOME)/.local/bin/vi-mode.sh on
 	echo "jumpstart vbase added OK"
