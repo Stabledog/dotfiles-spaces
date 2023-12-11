51fb9ad048f0f36c83c985a946fc2aaaa969c966
LES MATHESON <LMATHESON4@bloomberg.net>
2023-12-11 15:52:51 +0000

    Remove errant brace in vimsane.mk

--- inc/vimsane.mk
+++ inc/vimsane.mk
@@ -9,7 +9,7 @@ $(Flag)/vimsane: $(Finit)
 	clone_url=https://github.com/sanekits/vimsane
 	$(ISBB) && clone_url=bbgithub:sanekits/vimsane
 	git clone $$clone_url ./vimsane.tmp
-	}
+
 	cd ./vimsane.tmp
 	make setup
 	cd ..
