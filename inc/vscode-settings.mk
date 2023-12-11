cf23bf9aa0843f01bee460d638ae587932c6baac
Les Matheson <lmatheson4@bloomberg.net>
2023-12-11 12:56:05 -0500

    Merge vsweb-settings into vscode-settings

--- inc/vscode-settings.mk
+++ inc/vscode-settings.mk
@@ -1,21 +1,54 @@
 inc/vscode-settings.mk: ;
 
 
+$(Flag)/vscode-settings-branchselect:
+	@# Target $@
+	# Priority 1: if there's a branch that matches our machine name?
+	set -x
+	cd $(VscodeUserDir)
+	host=$$(hostname | tr 'A-Z' 'a-z')
+	[[ -n "$$host" ]] && {
+		git checkout "$$host" && {
+			touch $@
+			exit 0
+		} || :
+	} || :
+	touch $@
+
+
 $(Flag)/vscode-repo:
 	@# Target $@
 	# clone or update the Roaming/Code/User dir from github
 	cd $(VscodeUserDir) || exit 21
-	[[ -d ./.git ]] || {
+	[[ -d ./.git ]] && {
 		git pull
 	} || {
 		git clone $(VscodeSettingsOrg)/vscode.settings tmp$$$$
 		mv tmp$$$$/.git ./
 		rm -rf tmp$$$$
+		git remote add ghmine $(GhPubOrg)/vscode.settings.git
 	}
+	# Let's do ./snippets also:
+	mkdir -p ./snippets
+	cd ./snippets
+	[[ -d ./.git ]] && {
+		git pull
+	} || {
+		git clone $(VscodeSettingsOrg)/vscode.snippets tmp$$$$
+		mv tmp$$$$/.git ./
+		rm -rf tmp$$$$
+		git remote add ghmine $(GhPubOrg)/vscode.snippets.git
+	}
+	echo "$$PWD 1" >> $(HOME)/.tox-index
+
 	touch $@
 
-$(Flag)/vscode-settings:  $(Finit) $(Flag)/vscode-repo
-	exit 99 # Unfinished target
+$(Flag)/vscode-settings:  \
+	$(Finit) \
+	$(Flag)/vscode-repo \
+	$(Flag)/vscode-settings-branchselect
+
+	touch $@
 
 
 vscode-settings: $(Flag)/vscode-settings
