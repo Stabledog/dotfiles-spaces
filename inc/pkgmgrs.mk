c2eca8838260fcc4ed4f543ed94f4b608a50ecc2
Les Matheson <lmatheson4@bloomberg.net>
2023-12-27 06:11:59 -0500

    Clean up pkgmgrs include

--- inc/pkgmgrs.mk
+++ inc/pkgmgrs.mk
@@ -6,7 +6,7 @@ Config: .cfg-pkgmgrs
 # Environment detection is supposed to produce a PKG_MANAGERS list, and
 # each entry in that list is potentially a subdir of inc/pkgmgrs  -- e.g. if PKG_MANAGERS contains
 # 'apt-get', then we will try to  load inc/pkgmgrs/apt-get/*.mk:
--include $(addsuffix /*.mk, $(addprefix inc/pkgmgrs/, $(PKG_MANAGERS)))
+-include $(patsubst %,inc/pkgmgrs/%.mk,$(PKG_MANAGERS))
 
 
 .cfg-pkgmgrs:
