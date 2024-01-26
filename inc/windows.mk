1c3cfb7103c68ddd24d10bb1756e7ff45bdeb786
Les Matheson <lmatheson4@bloomberg.net>
2024-01-26 09:37:35 -0500

    Add windows-wsl-integration target

--- inc/windows.mk
+++ inc/windows.mk
@@ -3,7 +3,7 @@ inc/windows.mk: ;
 $(Flag)/windows-env.sh:
 	@# Target $@
 	# $(Flag)/windows-env.sh can be sourced to get linuxified paths for
-	# the main Windows environment variable
+	# the main Windows environment variables
 	set -x
 	tmpfile=$(VHOME)/.tmp-windows-env-$$$$
 	which cmd.exe || exit 19
@@ -13,6 +13,18 @@ $(Flag)/windows-env.sh:
 	<$$tmpfile tr '\\' '/' | tr -d '\r' | sed -E -e 's,C\:,/mnt/c,' >$@
 
 	rm $$tmpfile
+
+$(Flag)/windows-wsl-integration:
+	@# Stuff to help with hooking up shell to Windows
+	[[ $(DOTFILES_SYS) == wsl ]] && {
+		$(MAKE) -s $(Flag)/windows-env.sh
+		source $(Flag)/windows-env.sh
+		# ^^ That provides values like USERPROFILE APPDATA etc
+		cd && ln -sf $${USERPROFILE} win-profile
+		cd && ln -sf $${USERPROFILE}/AppData appdata
+		cd && ln -sf $${USERPROFILE}/Downloads downloads
+	}
+	touch $@
 
 Config: .cfg-windows-env
 .cfg-windows-env:
