042bc84b54200bb453a050a7b2eef288481ccfdf
Les Matheson <lmatheson4@bloomberg.net>
2024-02-11 11:44:11 -0500

    Remove bb env customization for vimsane

--- inc/vimsane.mk
+++ inc/vimsane.mk
@@ -7,7 +7,6 @@ $(Flag)/vimsane: $(Finit)
 	cd $(VHOME)/tmp
 	[[ -d vimsane.tmp ]] && rm -rf ./vimsane.tmp
 	clone_url=https://github.com/sanekits/vimsane
-	$(ISBB) && clone_url=bbgithub:sanekits/vimsane
 	git clone $$clone_url ./vimsane.tmp
 
 	cd ./vimsane.tmp
