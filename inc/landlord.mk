bf540432f01472d0d49bc1e8d22acfaf91156ca8
Les Matheson <LMATHESON4@bloomberg.net>
2024-05-15 17:47:05 +0000

    Switch landlord to main

--- inc/landlord.mk
+++ inc/landlord.mk
@@ -4,7 +4,7 @@ landlord: $(Flag)/landlord
 $(Flag)/landlord: $(Finit)
 	@# Add landlord to the current env
 	cd $(HOME)
-	git clone bbgithub:TdocProgressMetrics/landlord -b bare-spaces
+	git clone bbgithub:TdocProgressMetrics/landlord
 	cd landlord
 	make setup
 	touch $@
