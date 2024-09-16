72749c1ef94c80a60a31a5ec21bd335b650e43c2
Les Matheson <LMATHESON4@bloomberg.net>
2024-09-16 21:55:09 +0000

    Add --force for color theme install

--- inc/vsweb-colorthemes.mk
+++ inc/vsweb-colorthemes.mk
@@ -3,6 +3,6 @@ inc/vsweb-colorthemes.mk: ;
 vsweb-colorthemes: $(Flag)/vsweb-colorthemes
 $(Flag)/vsweb-colorthemes: $(Finit) $(Flag)/vsweb-settings
 	@ #  Install our favorite color themes
-	$(Code) $(CodeOpts) --install-extension ahmadawais.shades-of-purple
-	$(Code) $(CodeOpts) --install-extension catppuccin.catppuccin-vsc-pack
+	$(Code) $(CodeOpts) --force --install-extension ahmadawais.shades-of-purple
+	$(Code) $(CodeOpts) --force --install-extension catppuccin.catppuccin-vsc-pack
 	touch $@
