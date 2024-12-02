8217a4a84de90eb82a75d91e82388a7f6b11d055
Les Matheson <LMATHESON4@bloomberg.net>
2024-12-02 22:45:16 +0000

    Add benign fail for vscode extension installs if vscode not hosting the terminal

--- inc/vscodevim.mk
+++ inc/vscodevim.mk
@@ -3,5 +3,9 @@ inc/vscodevim.mk: ;
 vscodevim: $(Flag)/vscodevim
 $(Flag)/vscodevim: $(Finit)
 	@# Install the vscode vim extension
+	[[ -n "$(VSCODE_IPC_HOOK_CLI)" ]] || {
+		echo "Unable to install vscode extensions: try again in a vscode terminal" >&2
+		exit 0
+	}
 	$(VscodeExtInstall) vscodevim.vim
 	touch $@
--- inc/vsweb-colorthemes.mk
+++ inc/vsweb-colorthemes.mk
@@ -3,5 +3,9 @@ inc/vsweb-colorthemes.mk: ;
 vsweb-colorthemes: $(Flag)/vsweb-colorthemes
 $(Flag)/vsweb-colorthemes: $(Finit) $(Flag)/vsweb-settings
 	@ #  Install our favorite color themes
+	[[ -n "$(VSCODE_IPC_HOOK_CLI)" ]] || {
+		echo "Unable to install vscode extensions: try again in a vscode terminal" >&2
+		exit 0
+	}
 	$(VscodeExtInstall) ahmadawais.shades-of-purple catppuccin.catppuccin-vsc-pack
 	touch $@
