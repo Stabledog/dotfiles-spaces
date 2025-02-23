2093a834bfbac58e8fc0ed644919ac24484c951d
Les Matheson <les.matheson@gmail.com>
2025-02-22 20:57:53 -0500

    pushd and popd were making bad noises

--- inc/my-home.mk
+++ inc/my-home.mk
@@ -3,13 +3,14 @@ inc/my-home.mk: ;
 
 -include $(VHOME)/my-home/dotfiles-spaces.mk
 $(VHOME)/my-home/dotfiles-spaces.mk: | $(SshDir)/dotfile-setup .env.mk
-	pushd $(VHOME)
-	@if $(ISBB); then
+	@
+	pushd $(VHOME) >/dev/null
+	if $(ISBB); then
 		git clone bbgithub:lmatheson4/my-home -o lm4
 	else
 		git clone git@github.com:Stabledog/my-home
 	fi
-	popd
+	popd >/dev/null
 	# my-home/dotfiles-spaces.mk takes it further:
 	[[ -f $@ ]] || exit 29
 	$(MAKE)  -s my-home-setup
