ffd0ff12557e7ccd1108e388fc8ac6ecdc519796
Les Matheson <lmatheson4@bloomberg.net>
2025-12-23 23:41:15 +0000

    Add reverse reconciliation reminder

--- git-pseudo-merge.sh
+++ git-pseudo-merge.sh
@@ -18,4 +18,4 @@ git checkout "$branch1" || exit 1
 # Overlay files from branch2 that exist in current working tree
 git ls-tree -r --name-only "$branch2" | xargs -r -I {} bash -c '[[ -e "{}" ]] && git checkout "'"$branch2"'" -- "{}"'
 
-echo "Pseudo-merge complete: $branch1 with files from $branch2"
+echo "Pseudo-merge complete: $branch1 with files from $branch2.  Remember to push after commit before reverse-reconciliation"
